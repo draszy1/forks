6d4078a9551089d971af78e3b22b98f724b81419
draszy1 <draszy1@gmail.com>
2021-01-26 16:45:58 +0100

    Initial Commit

---
+++ forks/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  forks
+//
+//  Created by Szymon Drawski on 26/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
