a7a095ba1df1197177600f8989a6089cdac6b4f4
draszy1 <draszy1@gmail.com>
2021-01-26 16:56:50 +0100

    init

--- forks/main.c
+++ forks/main.c
@@ -6,9 +6,24 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int counter = 0;
+    
+    int pidc1 = fork();
+    int pidc2 = fork();
+    
+    if (pidc1 == 0) {
+        printf("Child1: %\d\n", ++counter);
+    }
+    
+    if (pidc2 == 0) {
+        printf("Child2: %\d\n", ++counter);
+    }
+    
+    printf("Parent: %d\n", counter);
+    
     return 0;
 }
